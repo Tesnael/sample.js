91c987cde18317f125c7369020a2f85b7a7f2b33
Theresa Marcus-Omoba <theresamarcus.omoba@gmail.com>
2023-02-17 14:18:41 +0100

    added features

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 console.log("i am hungry");
+console.log("friendship");
 
 
-console.log("i am hungry;
+
+console.log("i am hungry");
 console.log("i am hungry");
 console.log("i am hungry");
